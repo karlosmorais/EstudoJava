5c78f089711484df7db21c744ab69b6efa99c30d
Karlos Morais <karlosmorais@gmail.com>
2020-01-03 09:28:24 -0300

    Subindo Python

---
+++ estudo/sintaxe-variaveis-e-fluxo/src/TesteVariaveis.java
@@ -0,0 +1,14 @@
+
+public class TesteVariaveis {
+		
+	public static void main(String[]args) {
+		System.out.println("Ola novo teste");
+		
+		int idade;
+		idade = 37;
+		
+		System.out.println(idade);
+		
+	}
+
+}
